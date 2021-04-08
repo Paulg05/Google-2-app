2c7e1bee439246ceaa07795e188e40a847ad9c19
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-04-08 11:51:33 -0700

    added avatar and viewgrid icons inside of header, completing header portion of the build

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -17,15 +19,15 @@ export default function Home() {
           <p className="link">Gmail</p>
           <p className="link">Images</p>
 
-        {/* icon */}
+          {/* icon */}
 
-
-        {/* avatar */}
-          <Avatar />
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+          <Avatar url="https://bit.ly/3sX6uSB" />
         </div>
       </header>
 
       {/* Body */}
+      
 
       {/* Footer */}
     </div>
