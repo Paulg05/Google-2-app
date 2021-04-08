3a687d4d23f192b1e53f17f27a8c9fc103105b9f
Paul Gonzalez <robertgonzalez018@gmail.com>
2021-04-08 12:34:37 -0700

    started on the build of the body portion, includes the image, form and input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -27,7 +29,18 @@ export default function Home() {
       </header>
 
       {/* Body */}
-      
+      <form>
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+      </form>
 
       {/* Footer */}
     </div>
